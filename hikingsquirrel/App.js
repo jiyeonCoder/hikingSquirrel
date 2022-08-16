7449e0400f7f9ce83b446837589cf2795a03dfc2
jiyeonCoder <jiyeon.choi218@gmail.com>
2022-08-17 01:37:03 +0900

    updated

--- hikingsquirrel/App.js
+++ hikingsquirrel/App.js
@@ -1,16 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './navigations/StackNavigator';
 import 'react-native-gesture-handler';
 
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+
+import Loading from './pages/Loading';
+
 export default function App() {
-  return (
+  const [ready, setReady] = useState(false);
+
+  const loadFont = () => {
+    setTimeout(async () => {
+      await Font.loadAsync({
+        Roboto: require('native-base/Fonts/Roboto.ttf'),
+        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+        ...Ionicons.font,
+      });
+      await setReady(true);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    loadFont();
+  }, []);
+
+  return ready ? (
     <NavigationContainer>
       <StatusBar style="auto" />
       <StackNavigator />
     </NavigationContainer>
+  ) : (
+    <Loading />
   );
 }
 
