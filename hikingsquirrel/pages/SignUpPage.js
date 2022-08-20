3d27eb2708324853d25fcfa424020d0c5da4d546
jiyeonCoder <jiyeon.choi218@gmail.com>
2022-08-21 00:55:11 +0900

    updated

--- hikingsquirrel/pages/SignUpPage.js
+++ hikingsquirrel/pages/SignUpPage.js
@@ -1,13 +1,115 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, ImageBackground, View } from 'react-native';
+import {
+  Container,
+  Content,
+  Text,
+  Form,
+  Item,
+  Input,
+  Label,
+  Button,
+  Header,
+  Left,
+  Icon,
+  Body,
+  Right,
+} from 'native-base';
+import ItemInput from '../components/ItemInput';
+
+const bImage = require('../assets/background.png');
 
 export default function SignUpPage({ route }) {
   return (
-    <View>
-      <Text>SignUpPage</Text>
-      <Text>{route.params.title}</Text>
-    </View>
+    <Container>
+      <ImageBackground source={bImage} style={styles.backgroundImage}>
+        <Header transparent>
+          <Left>
+            <Button
+              transparent
+              onPress={() => {
+                navigation.goBack();
+              }}
+            >
+              <Icon name="arrow-back" style={{ color: '#fff' }} />
+            </Button>
+          </Left>
+          <Body />
+          <Right />
+        </Header>
+        <Content contentContainerStyle={styles.content} scrollEnabled={true}>
+          <Text style={styles.title}>
+            <Text style={styles.highlite}>Hiking</Text>Squirrel Signup
+          </Text>
+          <Form style={styles.form}>
+            <ItemInput title={'Nickname'} />
+            <ItemInput title={'Email'} />
+            <ItemInput title={'Password'} />
+            <ItemInput title={'Confirm Password'} />
+          </Form>
+          <Button full style={styles.emailSignIn}>
+            <Text>Sign Up</Text>
+          </Button>
+        </Content>
+      </ImageBackground>
+      <Content padder></Content>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(52, 52, 52, 0.5)',
+    margin: 20,
+    borderRadius: 20,
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: '700',
+    color: '#fff',
+    textAlign: 'center',
+  },
+  highlite: {
+    fontSize: 25,
+    fontWeight: '700',
+    color: 'deeppink',
+    textAlign: 'center',
+  },
+  form: {
+    width: 250,
+    borderRadius: 10,
+    paddingBottom: 20,
+    paddingRight: 20,
+    paddingLeft: 20,
+    marginTop: 10,
+  },
+  label: {
+    color: '#fff',
+  },
+  input: {
+    color: '#fff',
+  },
+  emailSignIn: {
+    alignSelf: 'center',
+    width: 250,
+    marginTop: 5,
+    borderRadius: 10,
+    backgroundColor: '#333',
+  },
+  emailSignUp: {
+    alignSelf: 'center',
+    width: 250,
+    marginTop: 5,
+    borderRadius: 10,
+    backgroundColor: '#eee',
+    borderWidth: 1,
+    borderColor: '#333',
+  },
+});
