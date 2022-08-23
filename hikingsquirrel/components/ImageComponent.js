6528f3ae1d3851088cb354c5100bd93f6d592e72
jiyeonCoder <jiyeon.choi218@gmail.com>
2022-08-23 15:06:40 +0900

    updated

--- hikingsquirrel/components/ImageComponent.js
+++ hikingsquirrel/components/ImageComponent.js
@@ -1,79 +1,18 @@
 import React from 'react';
-import { StyleSheet, Image, TouchableOpacity } from 'react-native';
-import { Col, Row, Grid } from 'react-native-easy-grid';
-import { Icon, Text, Card, CardItem } from 'native-base';
+import { StyleSheet, Dimensions } from 'react-native';
 import ImageBlurLoading from 'react-native-image-blur-loading';
 
-const image = require('../assets/background2.png');
-const logo = require('../assets/logo.png');
+const imageWidth = Dimensions.get('window').width / 3;
 
-export default function ImageComponent({ navigation, content }) {
+export default function ImageComponent({ image }) {
   return (
-    <TouchableOpacity
-      onPress={() => {
-        navigation.navigate('DetailPage');
-      }}
-      style={styles.container}
-    >
-      <Card style={styles.card} transparent>
-        <CardItem transparent>
-          <ImageBlurLoading
-            withIndicator
-            thumbnailSource={{ uri: content.image }}
-            source={{ uri: content.image }}
-            style={styles.image}
-          />
-        </CardItem>
-        <CardItem style={{ marginTop: -10 }}>
-          <Grid>
-            <Col size={9}>
-              <Text numberOfLines={1} style={styles.title}>
-                {content.title}
-              </Text>
-              <Text style={[styles.grey, styles.writer]}>{content.author}</Text>
-            </Col>
-            <Col size={2}>
-              <Grid>
-                <Col>
-                  <Icon name="chatbox-outline" style={styles.grey} />
-                </Col>
-                <Col>
-                  <Icon name="heart-outline" style={styles.grey} />
-                </Col>
-              </Grid>
-            </Col>
-          </Grid>
-        </CardItem>
-      </Card>
-    </TouchableOpacity>
+    <ImageBlurLoading
+      withIndicator
+      thumbnailSource={{ uri: image }}
+      source={{ uri: image }}
+      style={{ width: imageWidth, height: imageWidth }}
+    />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    alignSelf: 'center',
-  },
-  card: {
-    width: '100%',
-    alignSelf: 'center',
-  },
-  image: {
-    height: 200,
-    width: '100%',
-    borderRadius: 10,
-  },
-  grey: {
-    color: 'grey',
-  },
-  writer: {
-    fontSize: 12,
-    color: 'grey',
-    marginLeft: 10,
-  },
-  title: {
-    fontWeight: '700',
-    fontSize: 15,
-    marginLeft: 10,
-  },
-});
+const styles = StyleSheet.create();
--- hikingsquirrel/pages/MyMainPage.js
+++ hikingsquirrel/pages/MyMainPage.js
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
 import { StyleSheet, Alert } from 'react-native';
-import { Container, Content, Thumbnail, Text } from 'native-base';
+import { Container, Content, Thumbnail, Text, Grid, Col } from 'native-base';
 import HeaderComponent from '../components/HeaderComponent';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import ImageComponent from '../components/ImageComponent';
 
 const squirrel = require('../assets/squirrel.png');
+const data = require('../data.json');
 
 export default function MyMainPage({ navigation }) {
   useEffect(() => {
@@ -17,7 +20,30 @@ export default function MyMainPage({ navigation }) {
       <HeaderComponent />
       <Content>
         <Thumbnail large source={squirrel} style={styles.thumbnail} />
-        <Text style={styles.myTitle}>MySquirrel</Text>
+        <Text style={styles.myTitle}>My HikingSquirrel</Text>
+        <Text style={{ alignSelf: 'center' }}>jiyeon.choi218@gmail.com</Text>
+        <TouchableOpacity>
+          <Text style={styles.signOut}>Sign Out</Text>
+        </TouchableOpacity>
+        <Grid style={{ marginTop: 30 }}>
+          <Col size={3} style={{ alignItems: 'center' }}>
+            <Text style={styles.category}>작성한 글</Text>
+            <Text style={styles.categoryContent}>7</Text>
+          </Col>
+          <Col size={3} style={{ alignItems: 'center' }}>
+            <Text style={styles.category}>Comments</Text>
+            <Text style={styles.categoryContent}>21</Text>
+          </Col>
+          <Col size={3} style={{ alignItems: 'center' }}>
+            <Text style={styles.category}>방문 횟수</Text>
+            <Text style={styles.categoryContent}>321</Text>
+          </Col>
+        </Grid>
+        <Grid style={styles.imageWrap}>
+          {data.diary.map((content, i) => {
+            return <ImageComponent image={content.image} key={i} />;
+          })}
+        </Grid>
       </Content>
     </Container>
   );
@@ -27,5 +53,35 @@ const styles = StyleSheet.create({
   thumbnail: {
     alignSelf: 'center',
     marginTop: 30,
+  },
+  myTitle: {
+    alignSelf: 'center',
+    marginTop: 10,
+    fontSize: 20,
+    fontWeight: '700',
+  },
+  signOut: {
+    alignSelf: 'center',
+    padding: 10,
+    borderColor: 'grey',
+    borderWidth: 1,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  category: {
+    fontWeight: '700',
+  },
+  categoryContent: {
+    color: 'deeppink',
+    fontWeight: '700',
+  },
+  imageWrap: {
+    flexWrap: 'wrap',
+    marginTop: 20,
+  },
+  image: {
+    height: 200,
+    width: '100%',
+    borderRadius: 10,
   },
 });
