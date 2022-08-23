cfc3b1e94ba547873298ea70ea2ea46309dbd0c0
jiyeonCoder <jiyeon.choi218@gmail.com>
2022-08-24 01:19:32 +0900

    updated

--- hikingsquirrel/pages/DetailPage.js
+++ hikingsquirrel/pages/DetailPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-export default function DetailPage() {
+export default function DetailPage(navigation) {
   return (
     <View>
       <Text>DetailPage</Text>
