44af474fec008e804676c4e491074f1219aee724
jiyeonCoder <jiyeon.choi218@gmail.com>
2022-08-27 01:37:21 +0900

    updated

--- hikingsquirrel/config/firebaseFunctions.js
+++ hikingsquirrel/config/firebaseFunctions.js
@@ -5,8 +5,15 @@ import { Alert } from 'react-native';
 export async function registration(nickName, email, password) {
   try {
     console.log(nickName, email, password);
+    //Create a new user
     await firebase.auth().createUserWithEmailAndPassword(email, password);
     const currentUser = firebase.auth().currentUser;
+    //Save the user's info in the Cloud Firestore
+    const db = firebase.firestore();
+    db.collection('users')
+      .doc(currentUser.uid)
+      .set({ email: currentUser.email, nickName: nickName });
+    console.log('CurrentUser: ');
     console.log(currentUser);
     Alert.alert('Signing Up Success!');
   } catch (err) {
